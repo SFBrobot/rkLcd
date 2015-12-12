a96705c9430c82b0d26c31d068bb618123e2526b
rk1024 <rookie1286@gmail.com>
2015-12-11 17:19:02 -0800

    First commit!  Woo!

---
+++ lib.h
@@ -0,0 +1,13 @@
+#ifndef RKLCD
+#define RKLCD
+
+void clearLCD() {
+	clearLCDLine(0);
+	clearLCDLine(1);
+}
+
+void sprintBatt(string str) {
+	sprintf(str, "%6.2f // %-6.2f", nImmediateBatteryLevel / 1000., BackupBatteryLevel / 1000.);
+}
+
+#endif
