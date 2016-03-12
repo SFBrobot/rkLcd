06740581eddaa7c5f75eb63ccd36c98d4b5f786d
rookie1024 <rookie1286@gmail.com>
2016-03-12 09:57:41 -0800

    Ran file through Visual Studio's formatter

--- lib.h
+++ lib.h
@@ -2,12 +2,12 @@
 #define RKLCD
 
 void clearLCD() {
-	clearLCDLine(0);
-	clearLCDLine(1);
+  clearLCDLine(0);
+  clearLCDLine(1);
 }
 
 void sprintBatt(string str) {
-	sprintf(str, "%6.2f // %-6.2f", nImmediateBatteryLevel / 1000., BackupBatteryLevel / 1000.);
+  sprintf(str, "%6.2f // %-6.2f", nImmediateBatteryLevel / 1000., BackupBatteryLevel / 1000.);
 }
 
 #endif
