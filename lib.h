3fed8a21bced2cf3715de97af62fda9748c415b6
rk1024 <rookie1286@gmail.com>
2016-04-13 16:58:09 -0700

    Add power expander functionality

--- lib.h
+++ lib.h
@@ -10,4 +10,8 @@ void sprintBatt(string str) {
   sprintf(str, "%6.2f // %-6.2f", nImmediateBatteryLevel / 1000., BackupBatteryLevel / 1000.);
 }
 
+void sprintBatt(string str, tSensors expndStatus) {
+  sprintf(str, " %2.2f|%2.2f|%2.2f", nImmediateBatteryLevel / 1000., BackupBatteryLevel / 1000., SensorValue[expndStatus] / 182.4);
+}
+
 #endif
